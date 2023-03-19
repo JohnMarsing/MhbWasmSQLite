9068ad561b0c79df8772edc2793364d6eb25c793
JohnMarsing <johnmarsing@gmail.com>
2023-03-19 13:28:01 -0700

    Imported all Fav Ver. content and finished index. Need to convert Content/*.razor to MD (I'm thinkng)

--- MhbWasmSQLite/Client/Features/FavoriteVerses/Index.razor.cs
+++ MhbWasmSQLite/Client/Features/FavoriteVerses/Index.razor.cs
@@ -1,16 +1,40 @@
 using Microsoft.AspNetCore.Components;
-using MhbWasmSQLite.Client.Services;
-using MhbWasmSQLite.Shared;
+//using MhbWasmSQLite.Client.Services;
+using MhbWasmSQLite.Client.Enums;
 
 namespace MhbWasmSQLite.Client.Features.FavoriteVerses;
 
 public partial class Index
 {
 	[Inject] public ILogger<Index>? Logger { get; set; }
-	[Inject] public IScriptureService? svc { get; set; }
+	//[Inject] public IScriptureService? svc { get; set; }
 
-	public IList<FavoriteVerse>? Favorites { get; set; }
+	protected Status _status;
+	protected string _msg = string.Empty;
 
-	//public FavoriteVerseCategory Category { get; set; }
+	protected List<FavoriteVerse>? Favorites;
 
-}
+	protected override void OnInitialized()
+	{
+		base.OnInitialized();
+		Logger!.LogDebug(string.Format("Inside {0}"
+			, nameof(Index) + "!" + nameof(OnInitialized)));
+
+
+		Favorites = FavoriteVersesFactory.GetAll().ToList();
+		try
+		{
+			_status = Status.Loading;
+			foreach (var item in Favorites)
+			{
+			}
+			_status = Status.Loaded;
+		}
+		catch (Exception ex)
+		{
+			_status = Status.Error;
+			_msg = ex.ToString();
+		}
+	}
+}		
+
