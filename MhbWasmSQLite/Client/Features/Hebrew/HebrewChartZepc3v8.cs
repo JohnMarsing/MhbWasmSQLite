5c731d7b5b559bfd1c644182d1f16ae8f27f73cc
JohnMarsing <johnmarsing@gmail.com>
2023-03-31 15:11:40 -0700

    Finished Zep 3:8

--- MhbWasmSQLite/Client/Features/Hebrew/HebrewChartZepc3v8.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MhbWasmSQLite.Client.Features.Hebrew;
-
-public class HebrewChartZepc3v8
-{
-	public int WordCount { get; set; }
-	public string? Translation { get; set; }
-	public string? PRS { get; set; }
-}
