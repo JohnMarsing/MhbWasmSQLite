710c89aeb9f60d523634e36226d934c6c1cc1d14
JohnMarsing <johnmarsing@gmail.com>
2023-03-31 13:03:55 -0700

    Created generic LearnMoreToggleTemplate and added example in Home!Index which I wrapped with FooterLearnMore.razor

---
+++ MhbWasmSQLite/Client/Components/LearnMoreToggleTemplate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MhbWasmSQLite.Client.Components;
+
+public partial class LearnMoreToggleTemplate
+{
+	[Parameter] public RenderFragment? ChildContent { get; set; }
+
+	public bool ContentIsShowing = false;
+	private string Color => ContentIsShowing ? "btn-primary" : "btn-primary";
+	private string Title => ContentIsShowing ? "Hide " : "Learn more...";
+	private string Icon => ContentIsShowing ? "fas fa-chevron-up" : "fas fa-chevron-down";
+
+	protected void LearnMoreToggleButtonClick()
+	{
+		ContentIsShowing = !ContentIsShowing;
+	}
+
+}
