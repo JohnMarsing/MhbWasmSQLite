9e298074ffe471903f2f62bff79ead7df2b9a76d
JohnMarsing <johnmarsing@gmail.com>
2023-03-31 08:52:27 -0700

    Ported Menubar[MdLgXs] to SubNavBar which uses bootstrap <nav>...</nav> thus eliminating the need for a MediaQuery parameter

---
+++ MhbWasmSQLite/Client/Features/Hebrew/Components/SubNavBar.razor.cs
@@ -0,0 +1,28 @@
+﻿using MhbWasmSQLite.Client.Features.Hebrew.Enums;
+using Microsoft.AspNetCore.Components;
+
+namespace MhbWasmSQLite.Client.Features.Hebrew.Components;
+
+public partial class SubNavBar
+{
+	bool collapseNavMenu = true;
+	string? SubNavBarCssClass => collapseNavMenu ? "collapse" : null;
+	void ToggleSubNavBar()
+	{
+		collapseNavMenu = !collapseNavMenu;
+	}
+
+	// ToDo: review this note because `Display` isn't being used
+	//   Can't force EditorRequired because PrintTable is set to `Display=> false`
+	[Parameter] public MenuItems? ActiveMenuItem { get; set; }
+	
+	// ToDo: this was here to theme switching to dark mode which has not happen yet, 
+	//   this could just be hard coded in SubNavBar.razor 
+	protected string NavBarColor = " navbar-light bg-light ";  
+
+	string GetActive(MenuItems currentMenuItem)
+	{
+		return ActiveMenuItem == currentMenuItem ? " active" : "";
+	}
+
+}
