6881c4729ec67c05fdc0c30071b29845e0b73dd2
JohnMarsing <johnmarsing@gmail.com>
2023-04-03 17:54:43 -0700

    Finally started working on Lists?

---
+++ MhbWasmSQLite/Client/Features/Lists/Constants.cs
@@ -0,0 +1,72 @@
+﻿namespace MhbWasmSQLite.Client.Features.Lists;
+
+//namespace MyHebrewBible.Web.Pages.List static class Pages
+public static class MenuItemConstants
+{
+	public static class Emendations
+	{
+		public const string Index = "/Emendations";
+		public const string Title = "Emendations";
+		public const string Icon = "fab fa-wordpress";
+	}
+
+	public static class GospelHarmony
+	{
+		public const string Index = "/GospelHarmony";
+		public const string Title = "GospelHarmony";
+		public const string Icon = "fab fa-wordpress";
+	}
+
+	public static class JotAndTittle
+	{
+		public const string Index = "/JotAndTittle";
+		public const string Title = "Jots and Tittles";
+		public const string Icon = "fab fa-wordpress";
+	}
+
+	public static class MapCanaanOT
+	{
+		public const string Index = "/MapCanaanOT";
+		public const string Title = "Map or Canaan OT";
+		public const string Icon = "fab fa-wordpress";
+	}
+
+	public const string TimeLine = "/BibleList/TimeLine";
+	public const string ParablesOfYeshua = "/BibleList/ParablesOfYeshua";
+	public const string YosephAndYeshuaComparison = "/BibleList/YosephAndYeshuaComparison";
+	public const string Feasts = "/BibleList/Feasts";
+	public const string PeopleKilled = "/BibleList/PeopleKilled";
+	public const string Mitzvot = "/BibleList/Mitzvot";
+
+	/*
+	- From  MhbWasmSQLite.Client.Features.Hebrew;
+
+		public static class Definitions
+		{
+			public const string Index = "/Definitions";
+			public const string Title = "Definitions";
+			public const string Icon = "fab fa-wordpress";
+		}
+	*/
+}
+/*
+
+	{
+
+	public static class PartialViews
+	{
+		public const string EmendationModal = "~/Pages/List/ListIntro/_EmendationModal.cshtml";
+		public const string EmendationContent = "~/Pages/List/ListIntro/_EmendationContent.cshtml";
+		public const string EmendationList = "_EmendationList";
+
+		public const string JotAndTittleModal = "~/Pages/List/ListIntro/_JotAndTittleModal.cshtml";
+		public const string JotAndTittleContent = "~/Pages/List/Listintro/_JotAndTittleContent.cshtml";
+		public const string JotAndTittleList = "_JotAndTittleList";  
+
+		public const string MitzvotModal = "~/Pages/List/ListIntro/_MitzvotModal.cshtml";
+		public const string MitzvotTable = "_MitzvotTable";
+		public const string MitzvotContent = "~/Pages/List/Listintro/_MitzvotContent.cshtml";
+
+	}
+
+*/
---
+++ MhbWasmSQLite/Client/Features/Lists/Enums/JotAndTittle.cs
@@ -0,0 +1,17 @@
+﻿namespace MhbWasmSQLite.Client.Features.Lists.Enums;
+
+public class JotAndTittle
+{
+	public int ID { get; set; }
+	public int ScriptureID { get; set; }
+	public string? Verse { get; set; }
+	public string? HTMLFormatEng { get; set; }
+	public string? Notes { get; set; }
+	public string? Slug
+	{
+		get
+		{
+			return Verse.Replace(" ", "-").Replace(":", "-").Replace(";", "-"); //
+		}
+	}
+}
